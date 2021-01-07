82602a5afc0e869a2c988356388f19308d9ff6fe
Lawrence Kay <me@lkaybob.pe.kr>
2021-01-07 22:33:44 +0900

    Init with AutoConfiguration example

---
+++ 03-dependency-management/autoconfigure-application/src/main/java/me/lkaybob/DemoApplication.java
@@ -0,0 +1,26 @@
+package me.lkaybob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+
+//    과연 이걸 다 정의해야할까? (properties 를 정의해서 할 수 있음!)
+//    @Bean
+//    public Holoman holoman() {
+//        Holoman holoman = new Holoman();
+//        holoman.setName("Kaybob");
+//        holoman.setHowLong(50);
+//        return holoman;
+//    }
+
+}
