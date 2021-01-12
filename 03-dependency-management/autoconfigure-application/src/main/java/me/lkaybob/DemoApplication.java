b61cd8e4ca72fd6d357750946eb7b7ba3169f373
Lawrence Kay <me@lkaybob.pe.kr>
2021-01-13 00:06:29 +0900

    HTTPS & HTTP2 demo * Certificate generation needed (See the lecture note)

--- 03-dependency-management/autoconfigure-application/src/main/java/me/lkaybob/DemoApplication.java
+++ 03-dependency-management/autoconfigure-application/src/main/java/me/lkaybob/DemoApplication.java
@@ -3,7 +3,6 @@ package me.lkaybob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
---
+++ 03-https-and-http2/spring-https-demo/src/main/java/me/lkaybob/springhttpsdemo/SpringHttpsDemoApplication.java
@@ -0,0 +1,37 @@
+package me.lkaybob.springhttpsdemo;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringHttpsDemoApplication {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHttpsDemoApplication.class, args);
+    }
+
+}
