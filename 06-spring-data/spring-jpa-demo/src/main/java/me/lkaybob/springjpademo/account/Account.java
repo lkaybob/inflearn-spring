da7ab1cd42e44938f41993cd8f3de28012b51959
Lawrence Kay <hello@lkaybob.pe.kr>
2021-03-01 16:07:26 +0900

    Database migration with Flyway

--- 06-spring-data/spring-jpa-demo/src/main/java/me/lkaybob/springjpademo/account/Account.java
+++ 06-spring-data/spring-jpa-demo/src/main/java/me/lkaybob/springjpademo/account/Account.java
@@ -7,10 +7,30 @@ import javax.persistence.Id;
 
 @Entity
 public class Account {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     private String username;
     private String password;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    private boolean active;
 
     public Long getId() {
         return id;
